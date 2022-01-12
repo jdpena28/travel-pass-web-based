7213bcf01f2ca146cf5678ece0d9eb97622ca408
jdpena28 <johnhenrich1128@gmail.com>
2022-01-12 08:20:47 +0800

    Initialize Vue.js Boilerplate

---
+++ components/ButtonLogIn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="w-max rounded-xl p-[3px] bg-gradient-to-r from-blue-600 to-violet-600">
+    <div :class="className">
+      <img
+        :src="require(`~/assets/images/logo/${logo}.svg`)"
+        :alt="alt"
+        height="24px"
+        width="24px"
+      />
+      <p class="font-bold">{{btnText}}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: "ButtonLogIn",
+    props:[
+        "btnText",
+        "logo",
+        "alt",
+        "className",
+    ]
+}
+</script>
+<style lang=""></style>
