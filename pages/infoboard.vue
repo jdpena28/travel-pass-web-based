1758c74392bccefb7029505b4a28a2457241eecf
jdpena28 <johnhenrich1128@gmail.com>
2022-01-14 11:40:08 +0800

    Add infoboard page

---
+++ pages/infoboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <section
+      id="info-board"
+      class="h-screen w-full bg-cover">
+      <div class="container mx-auto ">
+        <!--insert your code here-->
+        <h1 class="text-xl font-semibold">Insert your code here remove this as necessary</h1>
+      </div>
+    </section>
+</template>
+<script>
+export default {
+    name:'InformationBoard'
+}
+</script>
+<style scoped>
+    
+</style>
