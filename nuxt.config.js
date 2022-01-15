ad45b078df7b9322815bf7462f4c45acc6de8a97
jdpena28 <johnhenrich1128@gmail.com>
2022-01-15 21:00:24 +0800

    Add logo of 1bataan

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'travelpass-system',
+    title: '1Bataan | Travel Pass System',
     htmlAttrs: {
       lang: 'en',
     },
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/png', href: '/1bataan-logo-small-1.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
