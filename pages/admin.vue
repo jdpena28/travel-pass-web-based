3d84865943d8844892dd0b96690a195bb2124f10
rtroy91 <rtbocasiones@bpsu.edu.ph>
2022-01-22 18:46:14 +0800

    BUGFIX #33 AdminPage: Alignment

--- pages/admin.vue
+++ pages/admin.vue
@@ -2,7 +2,7 @@
   <section id="admin-page" class="h-screen w-full bg-cover bg-[#0C68ED]">
     <div class="w-[95%] flex justify-between items-center absolute mx-4">
       <img
-        class="w-[20%] justify-center items-center ml-24"
+        class="w-[20%] justify-center items-center ml-20"
         src="~/assets/images/dashboard-logo.png"
         alt="Dashboard Logo"
         height="720px"
@@ -16,7 +16,7 @@
         </h2>
       </div>
       <img
-        class="w-[4%] justify-center items-center mr-8"
+        class="w-[4%] justify-center items-center mr-4"
         src="~/assets/images/icon.png"
         alt="Icon"
         height="1080px"
@@ -26,11 +26,11 @@
       class="container w-full h-screen flex justify-end items-center mx-auto">
       <div class="flex-col justify-center items-center">
         <button
-          class="bg-blue-700 border-2 hover:bg-blue-500 text-white font-bold py-2 px-[5.3rem] rounded-full">
+          class="bg-blue-700 border-2 hover:bg-blue-500 text-white font-bold py-2 px-[5.5rem] rounded-full">
           Approved
         </button>
         <button
-          class="bg-blue-700 border-2 hover:bg-blue-500 text-white font-bold mt-4 py-2 px-[5.9rem] rounded-full">
+          class="bg-blue-700 border-2 hover:bg-blue-500 text-white font-bold mt-4 py-2 px-[5.8rem] rounded-full">
           Rejected
         </button>
       </div>
@@ -41,19 +41,19 @@
           type="search"
           placeholder="Search..." />
         <button
-          class="absolute w-[20%] left-[91%] top-[3%] mx-auto"
+          class="absolute w-[13%] left-[91%] top-[3.5%] mx-auto"
           type="button">
           <img
-            class="absolute w-[2.5rem] rounded-full"
+            class="absolute w-[2rem] rounded-full"
             src="~/assets/images/search-icon.svg"
             alt="Ticket Logo"
             height="1980px"
             width="1080px" />
         </button>
         <div
-          class="w-[92%] h-[80%] mt-20 mr-[4%] absolute rounded-3xl bg-gray-100"></div>
+          class="w-[92%] h-[77%] mt-20 mr-[4%] absolute rounded-3xl bg-gray-100"></div>
         <button
-          class="absolute bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 w-[22%] rounded-full left-[74%] bottom-[3%]">
+          class="absolute bg-blue-700 hover:bg-blue-500 text-white text-[100%] font-bold mt-[1%] py-[1%] w-[22%] rounded-full left-[74%] bottom-[3%]">
           Refresh
         </button>
       </div>
